b56e557266289ceb855227864f73f0c3908f7cc3
左佳伟 <912489134@qq.com>
2019-09-09 14:11:05 +0800

    实体类

---
+++ day1_1/src/main/java/com/springboot/pojo/User.java
@@ -0,0 +1,24 @@
+package com.springboot.pojo;
+
+public class User {
+
+    private  Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private  String username;
+}
